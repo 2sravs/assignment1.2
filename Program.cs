d88c0f569753956e1e8c3a2ed68cdfe12c937ee2
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-12 15:08:15 +0530

    assignment

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conassign_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = "20";
+                int ConvertedNumber = Convert.ToInt32(input);
+                Console.WriteLine(",convert number is: \t" + ConvertedNumber);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
